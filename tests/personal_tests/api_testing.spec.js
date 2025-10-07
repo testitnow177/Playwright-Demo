f540b6c5b3af555835dbb24c4c4549e8172202a5
Ajay Kumar <testit.now177@gmail.com>
2025-10-07 17:47:33 +0530

    api testing 1.0.0

---
+++ tests/personal_tests/api_testing.spec.js
@@ -0,0 +1,48 @@
+const {test, expect} = require('@playwright/test');
+const { request } = require('http');
+
+ var userid;
+test("get user", async ({request})=> {
+    const response = await request.get("url");
+    console.log(response.json())
+
+    expect (response.status()).toBe(200)
+
+})
+
+test("Create user", async ({request})=> {
+    const response = await request.post("url",{
+
+        data :{
+            "name": "kumar",
+            "job": "trainer"
+        },
+        
+        headers:{
+
+            "Accept": "application/josn"
+        }
+    })
+    console.log(await response.json())
+    expect (response.status()).toBe(201)
+    
+    var res = await response.json()
+    userid = res.id
+})
+
+
+test("update user", async({request})=>{
+    await request.put("url"+userid, 
+        
+        {
+            data:{"name": "kumar",
+                  "job": "engineer"
+
+            },
+            headers:{
+                "Accept":"application.json"
+            }
+    })
+    console.log( await response.json());
+    expect(response.status()).tobe(200);
+})
